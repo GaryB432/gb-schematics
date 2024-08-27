72f0bb4a20208c7cee3ecad3bbbc81cb9e3462b6
Gary Bortosky <garybort@gmail.com>
2024-08-27 15:32:41 -0500

    disable prettier for eslint schematic

--- src/eslint/index.ts
+++ src/eslint/index.ts
@@ -14,6 +14,10 @@ import type { Options } from './schema';
 
 export function eslint(options: Options): Rule {
   return (tree: Tree, context: SchematicContext) => {
+    if (options.includePrettier) {
+      context.logger.error(`--prettier option is no longer supported.`);
+      options.includePrettier = false;
+    }
     const sts = url('./files');
 
     const templatedSource = apply(sts, [applyTemplates({ ...options })]);
@@ -21,38 +25,37 @@ export function eslint(options: Options): Rule {
     const packageJsonPath = './package.json';
 
     const json = tree.read(packageJsonPath);
-    if (json) {
-      const pkgJson = {
-        scripts: {
-          lint: `eslint .`,
-        },
-        devDependencies: {
-          '@eslint/js': '^9.9.1',
-          '@types/eslint__js': '^8.42.3',
-          eslint: '^9.9.1',
-          'eslint-plugin-gb': '^9.0.0',
-          'typescript-eslint': '^8.3.0',
-        },
-        peerDependencies: {
-          typescript: '^5.5.4',
-        },
-      };
+    if (!json) throw new Error('no package json');
+    const newThings = {
+      scripts: {
+        lint: `eslint .`,
+      },
+      devDependencies: {
+        '@eslint/js': '^9.9.1',
+        '@types/eslint__js': '^8.42.3',
+        eslint: '^9.9.1',
+        'eslint-plugin-gb': '^9.0.0',
+        'typescript-eslint': '^8.3.0',
+      },
+      peerDependencies: {
+        typescript: '^5.5.4',
+      },
+    };
 
-      const npk = JSON.parse(json.toString());
-      const scripts = { ...npk.scripts, ...pkgJson.scripts };
-      const devDependencies = {
-        ...npk.devDependencies,
-        ...pkgJson.devDependencies,
-      };
-      const peerDependencies = {
-        ...npk.peerDependencies,
-        ...pkgJson.peerDependencies,
-      };
-      npk.scripts = scripts;
-      npk.devDependencies = devDependencies;
-      npk.peerDependencies = peerDependencies;
-      tree.overwrite(packageJsonPath, JSON.stringify(npk, null, 2));
-    }
+    const npk = JSON.parse(json.toString());
+    const scripts = { ...npk.scripts, ...newThings.scripts };
+    const devDependencies = {
+      ...npk.devDependencies,
+      ...newThings.devDependencies,
+    };
+    const peerDependencies = {
+      ...npk.peerDependencies,
+      ...newThings.peerDependencies,
+    };
+    npk.scripts = scripts;
+    npk.devDependencies = devDependencies;
+    npk.peerDependencies = peerDependencies;
+    tree.overwrite(packageJsonPath, JSON.stringify(npk, null, 2));
 
     // const rule0: Rule = (tree: Tree) => {
     //   tree.create('test', 'test');
@@ -64,7 +67,7 @@ export function eslint(options: Options): Rule {
       branchAndMerge(
         chain([
           tree.exists('./tsconfig.json') ? noop() : schematic('typescript', {}),
-          schematic('prettier', {}),
+          options.includePrettier ? schematic('prettier', {}) : noop(),
           mergeWith(templatedSource, MergeStrategy.Overwrite),
           // mergeWith(rule0, MergeStrategy.Overwrite)
         ]),
