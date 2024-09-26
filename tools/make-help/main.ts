9f36b9a7ad3f9a90c1ad72741a34f1e730cb3b02
Gary Bortosky <garybort@gmail.com>
2024-09-26 10:56:36 -0500

    stub make-help tool

---
+++ tools/make-help/main.ts
@@ -0,0 +1,7 @@
+export function greet(name: string): string {
+  return `main says hello to ${name}`;
+}
+export function add(a: number, b: number): number {
+  return a + b;
+}
+export const meaning: number = 2;
---
+++ tools/make-help/markdown/doc.ts
@@ -0,0 +1,10 @@
+export class Doc {
+  public constructor(private counter: number) {}
+  public add(a: number): number {
+    this.counter += a;
+    return this.counter;
+  }
+  public greet(name: string): string {
+    return `Doc says: hello to ${name}`;
+  }
+}
