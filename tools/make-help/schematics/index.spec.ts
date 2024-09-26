5fc955c217bac5dd06887b0bc4e347485aea8155
Gary Bortosky <garybort@gmail.com>
2024-09-26 13:27:16 -0500

    trying more jasmine

--- tools/make-help/schematics/index.spec.ts
+++ tools/make-help/schematics/index.spec.ts
@@ -5,7 +5,7 @@ describe('Schematics module', () => {
     expect(add(2, 3)).toEqual(5);
   });
   it('greets', () => {
-    expect(greet('world')).toEqual('index says: hello to world');
+    expect(greet('world')).toEqual('index says: hello to worldx');
   });
   it('meaning', () => {
     expect(meaning.life).toEqual(42);
