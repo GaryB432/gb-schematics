d08af06be267998bb8d425fe33223dd909913572
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2025-03-18 13:19:21 -0500

    Clean up old schematics (#25)
    * delist old schematics
    
    * prune old schematics
    
    ---------
    
    Co-authored-by: Gary Bortosky <garyb432@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 export { Options as GbBumpOptions } from './bump/schema';
-export { Options as GbDevopsOptions } from './devops/schema';
-export { Options as GbEslintOptions } from './eslint/schema';
 export { Options as GbModuleOptions } from './module/schema';
 export { Options as GbSveltekitComponentOptions } from './sveltekit-component/schema';
 export { Options as GbSveltekitRouteOptions } from './sveltekit-route/schema';
