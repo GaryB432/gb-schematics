267fee48903e67ce7e8c6be3aa3023608387e5d5
Gary Bortosky <997067+GaryB432@users.noreply.github.com>
2026-09-22 01:48:51 -0500

    feat: enhance exclamation

--- packages/gen/app/main.ts
+++ packages/gen/app/main.ts
@@ -3,5 +3,5 @@ import { outro } from '@clack/prompts';
 export async function main(rawArguments: string[]): Promise<void> {
   // eslint-disable-next-line no-console
   console.log(rawArguments);
-  outro('coming soon!');
+  outro('coming soon!!');
 }
